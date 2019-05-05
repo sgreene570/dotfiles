511f4117c54ee472b932dc739b3ff765219dc7a6
sgreene570 <sgreene570@gmail.com>
2019-05-05 16:19:09 -0400

    Add simple bash relocator

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp bashrc ~/.bashrc
+cp bash_profile ~/.bash_profile
+cp inputrc ~/.inputrc
+cp nvim ~/.config/
+cp openbox/* ~/.config/openbox/
+cp tint2rc ~/.config/tint2/
+cp Xresources .Xresources
