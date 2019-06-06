0571c29545497be281cab17717b61d0c12cd59d3
sgreene570 <sgreene570@gmail.com>
2019-06-05 20:02:46 -0700

    Fix directory stuff in installer

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
 cp inputrc ~/.inputrc
 cp nvim ~/.config/
+mkdir ~/.config/openbox
 cp openbox/* ~/.config/openbox/
+mkdir ~/.config/tint2
 cp tint2rc ~/.config/tint2/
 cp Xresources .Xresources
